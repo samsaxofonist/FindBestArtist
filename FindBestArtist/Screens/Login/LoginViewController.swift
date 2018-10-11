20f8ed10b1c2fd09d8ecf51110ad955f4fcd9bc1
Andrii Kravchenko <andrii.kravchenko@goeuro.com>
2018-10-11 21:00:29 +0200

    Initial setup

---
+++ FindBestArtist/Screens/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  FindBestArtist
+//
+//  Created by Andrii Kravchenko on 11.10.18.
+//  Copyright © 2018 Samus Dimitrij. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
