4d2b7a3d15958be25732931affef747ac7e0ea38
Andrii Kravchenko <andrii.kravchenko@goeuro.com>
2019-03-05 16:36:27 +0100

    InformationUserController refactoring

--- BestArtist/Screens/Profile/InformationUserViewController.swift
+++ BestArtist/Screens/Profile/InformationUserViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MKDropdownMenu
 
-class InformationUserViewController: BaseViewController, UITextViewDelegate, UITextFieldDelegate, MKDropdownMenuDelegate, MKDropdownMenuDataSource {
+class InformationUserViewController: BaseViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var nameTitleLabelTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var informationAboutYourselfView: UITextView!
@@ -19,25 +19,11 @@ class InformationUserViewController: BaseViewController, UITextViewDelegate, UIT
     
     let talents = ["Singer", "DJ", "Saxophone", "Piano", "Moderation", "Photobox", "Photo", "Video"]
     var selectedRole: String?
-    
     var artist: Artist!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        nameTextField.addTarget(self, action: #selector(textFieldDidChange(sender:)), for: .editingChanged)
-        
-        nameTextField.becomeFirstResponder()
-        informationAboutYourselfView.delegate = self
-        nameTextField.delegate = self
-       
-        informationAboutYourselfView.text = "Artist of the original genre ..."
-        informationAboutYourselfView.textColor = UIColor.lightGray
-        
-        nameTextField.text = artist.name
-        informationAboutYourselfView.text = artist.description
-        selectedRole = artist.talent
+        setup()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -56,6 +42,52 @@ class InformationUserViewController: BaseViewController, UITextViewDelegate, UIT
     @objc func keyBoardWillHide(notification: NSNotification) {
         nameTitleLabelTopConstraint.constant = 21
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextViewController = segue.destination as! GetVideoViewController
+        artist.name = nameTextField.text ?? ""
+        artist.talent = selectedRole ?? ""
+        artist.description = informationAboutYourselfView.text
+        nextViewController.artist = artist
+    }
+}
+
+extension InformationUserViewController {
+    func setup() {
+        setupNotifications()
+        setupTextFields()
+        selectedRole = artist.talent
+    }
+    
+    func setupNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func setupTextFields() {
+        nameTextField.addTarget(self, action: #selector(textFieldDidChange(sender:)), for: .editingChanged)
+        nameTextField.becomeFirstResponder()
+        nameTextField.delegate = self
+        nameTextField.text = artist.name
+        
+        informationAboutYourselfView.delegate = self
+        informationAboutYourselfView.text = "Artist of the original genre ..."
+        informationAboutYourselfView.textColor = UIColor.lightGray
+        informationAboutYourselfView.text = artist.description
+    }
+    
+    func updateInfoCharacterCount() {
+        let numberCharacters = self.informationAboutYourselfView.text.count
+        self.numberCharactersLabel.text = "\((0) + numberCharacters)/500"
+    }
+    
+    func updateNameCharacterCount() {
+        let numberCharacters = self.nameTextField.text?.count ?? 0
+        self.numberNameLabel.text = "\((0) + numberCharacters)/10"
+    }
+}
+
+extension InformationUserViewController: MKDropdownMenuDelegate, MKDropdownMenuDataSource {
     
     func numberOfComponents(in dropdownMenu: MKDropdownMenu) -> Int {
         return 1
@@ -80,6 +112,32 @@ class InformationUserViewController: BaseViewController, UITextViewDelegate, UIT
     func dropdownMenu(_ dropdownMenu: MKDropdownMenu, titleForRow row: Int, forComponent component: Int) -> String? {
         return talents[row]
     }
+}
+
+extension InformationUserViewController: UITextFieldDelegate {
+    
+    @objc func textFieldDidChange(sender textField: UITextField) {
+        self.updateNameCharacterCount()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let numberSymbolsBefore = textField.text?.count ?? 0
+        let numberSymbolToAdd = string.count - range.length
+        return numberSymbolsBefore + numberSymbolToAdd <= 10
+    }
+}
+
+extension InformationUserViewController: UITextViewDelegate {
+
+    func textViewDidChange(_ textView: UITextView) {
+        self.updateInfoCharacterCount()
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool{
+        let numberSymbolsBefore = textView.text.count
+        let numberSymbolToAdd = text.count - range.length
+        return numberSymbolsBefore + numberSymbolToAdd <= 500
+    }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.textColor == UIColor.lightGray {
@@ -93,43 +151,5 @@ class InformationUserViewController: BaseViewController, UITextViewDelegate, UIT
             textView.text = "Artist of the original genre ..."
             textView.textColor = UIColor.lightGray
         }
-    }
-    
-    func updateInfoCharacterCount() {
-        let numberCharacters = self.informationAboutYourselfView.text.count
-        self.numberCharactersLabel.text = "\((0) + numberCharacters)/500"
-    }
-    
-    func textViewDidChange(_ textView: UITextView) {
-        self.updateInfoCharacterCount()
-    }
-    
-    func updateNameCharacterCount() {
-        let numberCharacters = self.nameTextField.text?.count ?? 0
-        self.numberNameLabel.text = "\((0) + numberCharacters)/10"
-    }
-    
-    @objc func textFieldDidChange(sender textField: UITextField) {
-        self.updateNameCharacterCount()
-    }
-    
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool{
-        let numberSymbolsBefore = textView.text.count
-        let numberSymbolToAdd = text.count - range.length
-        return numberSymbolsBefore + numberSymbolToAdd <= 500
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let numberSymbolsBefore = textField.text?.count ?? 0
-        let numberSymbolToAdd = string.count - range.length
-        return numberSymbolsBefore + numberSymbolToAdd <= 10
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let nextViewController = segue.destination as! GetVideoViewController
-        artist.name = nameTextField.text ?? ""
-        artist.talent = selectedRole ?? ""
-        artist.description = informationAboutYourselfView.text
-        nextViewController.artist = artist
     }
 }
