5bd03de9c2d820511f91d07c244eb8ebe63829a8
Andrii Kravchenko <andrii.kravchenko@goeuro.com>
2019-07-04 19:37:25 +0200

    Start to build MyProfile

--- BestArtist/Screens/My Profile/MyProfileViewController.swift
+++ BestArtist/Screens/My Profile/MyProfileViewController.swift
@@ -7,18 +7,37 @@
 //
 
 import UIKit
+import MKDropdownMenu
+import ImageSlideshow
+import KDCalendar
 
-class MyProfileViewController: BaseTableViewController {
+class MyProfileViewController: UITableViewController {
 
+    @IBOutlet weak var photoBackgroundVIew: UIView!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var artistTypeMenu: MKDropdownMenu!
+    
+    @IBOutlet weak var cityButton: UIButton!
+    @IBOutlet weak var priceButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBOutlet weak var photosSlideShow: ImageSlideshow!
+    
+    @IBOutlet weak var videosCollectionView: UICollectionView!
+    @IBOutlet weak var feedbacksCollectionVIew: UICollectionView!
+    
+    @IBOutlet weak var calendarView: CalendarView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-  
+    
+    @IBAction func editButtonClicked(_ sender: Any) {
+    }
+    
 }
+
