0fe54c5a34dbd16c838fa147056353d363c8536d
Дмитрий Самусь <samsaxofonist@gmail.com>
2020-03-18 09:57:12 +0100

    Moi Muchenija s dizajnom

--- BestArtist/Common/Themes.swift
+++ BestArtist/Common/Themes.swift
@@ -17,9 +17,9 @@ protocol Theme {
 }
 
 class ThemeOlive: Theme {
-    var backgroundColor: UIColor = UIColor(rgb: 0xFFFFFF)
-    var textColor: UIColor = UIColor(rgb: 0x6F6F6F)
-    var darkColor: UIColor = UIColor(rgb: 0x000000)
+    var backgroundColor: UIColor = UIColor(rgb: 0x3D465A)
+    var textColor: UIColor = UIColor(rgb: 0x8996A7)
+    var darkColor: UIColor = UIColor(rgb: 0x404040)
     var gradientStartColor: UIColor = UIColor(red: 0/255, green: 179/255, blue: 241/255, alpha: 1.0)
     var gradientEndColor: UIColor = UIColor(red: 12/255, green: 54/255, blue: 153/255, alpha: 1.0)
 }
